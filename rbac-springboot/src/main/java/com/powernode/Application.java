b883a34be9a2cfa52826de728780845331144836
zero199625 <1603670090@qq.com>
2023-03-24 23:18:26 +0800

    第四次提交  提交给v1.1

--- rbac-springboot/src/main/java/com/powernode/Application.java
+++ rbac-springboot/src/main/java/com/powernode/Application.java
@@ -11,11 +11,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
     }
 
 }
